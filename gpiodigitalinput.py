d8519705813006f684ae6a21fe9797c0f5d8c36e
aoarceb <125314677+aoarceb@users.noreply.github.com>
2026-01-15 23:39:16 -0500

    Update gpiodigitalinput.py
    script basico que imprime el estado del pin de entrada 10 (ON OFF)

--- gpiodigitalinput.py
+++ gpiodigitalinput.py
@@ -1,11 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+#from time import sleep
+
+INPUTPIN = 10 #GPIO ALARMA1
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8,GPIO.OUT)
+GPIO.setup(10,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+#Ahorita se encuentra el configuracion Pull Down
+#Si queremos cambiar de configuracion a Pull Up
+#sera necesario cambiar a: GPIO.PUD_UP
+
 estado_anterior = None
 
 try:
     while True:
-        estado = "ON" if GPIO.input(PIN) == GPIO.LOW else "OFF"
+        estado = "ON" if GPIO.input(INPUTPIN) == GPIO.HIGH else "OFF"
 
         if estado != estado_anterior:
-            client.publish(TOPIC, estado, qos=1)
+            #client.publish(TOPIC, estado, qos=1)
             print("Estado enviado:", estado)
             estado_anterior = estado
 
